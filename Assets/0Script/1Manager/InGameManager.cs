712c0514378025550930dd4fc89c5a091df2e7e5
a0966891512@gmail.com <a0966891512@gmail.com>
2024-07-13 01:59:19 +0900

    Sleep

--- Assets/0Script/1Manager/InGameManager.cs
+++ Assets/0Script/1Manager/InGameManager.cs
@@ -5,18 +5,41 @@ using UnityEngine;
 
 public class InGameManager : MonoBehaviour
 {
-    private List<int> _characters;            // キャラクターのリスト
-    private List<List<int>> _allStageEnemies; // 全部のステージの敵のリスト
-    private List<int> _nowStageEnemies;       // 現在のステージの敵のリスト
-    private List<int> _timeline;              // タイムラインのリスト
+    [SerializeField] private GameObject _cardPrefab;
+    [SerializeField] private GameObject _cardCanvas;        // プレイヤーのゲームオブジェクト
+    [SerializeField] private List<int> _characters;         // キャラクターのリスト
+    [SerializeField] private List<string> _allStageEnemies; // 全部のステージの敵のリスト
+    [SerializeField] private List<int> _nowStageEnemies;    // 現在のステージの敵のリスト
+    [SerializeField] private List<GameObject> _timeline;    // タイムラインのリスト
+    [SerializeField] private int _maxWindSpeed;
 
-    public void SetTimeline(List<int> timeline)
+    public List<int> Characters
     {
-        _timeline = timeline;
+        get => _characters;
+        set => _characters = value;
     }
 
-    public void SetStageEnemies(List<int> stageEnemies)
+    public List<string> AllStageEnemies
     {
-        _nowStageEnemies = stageEnemies;
+        get => _allStageEnemies;
+        set => _allStageEnemies = value;
+    }
+
+    public List<int> NowStageEnemies
+    {
+        get => _nowStageEnemies;
+        set => _nowStageEnemies = value;
+    }
+
+    public List<GameObject> Timeline
+    {
+        get => _timeline;
+        set => _timeline = value;
+    }
+
+    public int MaxWindSpeed
+    {
+        get => _maxWindSpeed;
+        set => _maxWindSpeed = value;
     }
 }

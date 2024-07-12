87b5c779890dc02553861ecba8cfce5dbea106d0
a0966891512@gmail.com <a0966891512@gmail.com>
2024-07-12 23:26:06 +0900

    File move AddPNG

--- Assets/0Script/1Manager/InGameManager.cs
+++ Assets/0Script/1Manager/InGameManager.cs
@@ -5,7 +5,18 @@ using UnityEngine;
 
 public class InGameManager : MonoBehaviour
 {
-    List<int> _timeline;
+    private List<int> _characters;            // キャラクターのリスト
+    private List<List<int>> _allStageEnemies; // 全部のステージの敵のリスト
+    private List<int> _nowStageEnemies;       // 現在のステージの敵のリスト
+    private List<int> _timeline;              // タイムラインのリスト
+
+    public void SetTimeline(List<int> timeline)
+    {
+        _timeline = timeline;
+    }
+
+    public void SetStageEnemies(List<int> stageEnemies)
+    {
+        _nowStageEnemies = stageEnemies;
+    }
 }
-
-
